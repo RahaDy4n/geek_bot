2758494a5bc3b0bfb64dde9916d979659336f699
RahaDy4n <77660894+RahaDy4n@users.noreply.github.com>
2021-01-21 15:22:18 +0800

    Add files via upload

---
+++ Lib/donasi.js
@@ -0,0 +1,32 @@
+exports.donasi = (id, geek-bot, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
+	return `🔰{ *MENU DONASI ${Geek-bot}* }🔰
+  
+  
+TERIMA KASIH
+*${id.split("@s.whatsapp.net")[0]}*
+TELAH MEMBUKA MENU DONASI
+🛡🛡
+
+
+🗓*${tampilTanggal}* 🛡
+⏰*${tampilWaktu}* 🛡
+(Waktu Server)
+
+KALIAN BISA DONASI MENGGUNAKAN
+   
+🛡 *PULSA*: 081271699856
+🛡 *OVO*: 081271699856
+🛡 *DANA* : 081271699866
+🛡 *GOPAY* : 081271699856
+
+📺*IKLAN* *RahaDyan*
+
+
+⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
+FOLLOW INSTAGRAM SAYA
+${'https://instagram.com/gem_in_anime?igshid=1b79rvyj9y5sk'} 
+
+👾TELEGRAM : ${ga punya}
+
+`
+}
